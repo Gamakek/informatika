278757f6d492987f805e16ba48c974e25d2e424c
Gamakek <149433644+Gamakek@users.noreply.github.com>
2025-09-29 11:10:51 +0300

    Add files via upload

---
+++ Gamakek.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() 
+{
+	float x, y;
+	cin >> x >> y;
+
+	float R = (-x + pow((pow(x, 2) - 4 * x * y), 1/2)) / (2 * x);
+	float S = (pow(4, -3 * x)) * sin(y);
+	cout << R << " " << S << "\n";
+
+	float C = max(R, S);
+	cout << C;
+	
+	cin.get();
+	return 0;
+}
