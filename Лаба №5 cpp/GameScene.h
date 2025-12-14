805c1cfc6e2ef71931e699bd327231b56e4c500d
Gamakek <149433644+Gamakek@users.noreply.github.com>
2025-12-14 15:09:15 +0300

    Add files via upload

---
+++ Лаба №5 cpp/GameScene.h
@@ -0,0 +1,24 @@
+﻿#ifndef GAMESCENE_H
+#define GAMESCENE_H
+
+#include "TmxLevel.h"
+
+struct GameScene
+{
+    TmxLevel level;
+    TmxObject player;
+    std::vector<TmxObject> enemies;
+    std::vector<TmxObject> coins;
+    std::vector<TmxObject> blocks;
+    sf::Vector2f startPosition;
+};
+
+GameScene* NewGameScene();
+
+void UpdateGameScene(void* pData, class GameView& view, float deltaSec);
+
+void DrawGameScene(void* pData, class GameView& view);
+
+void DestroyGameScene(GameScene*& pScene);
+
+#endif 
---
+++ Лаба №5 cpp/GameView.h
@@ -0,0 +1,25 @@
+﻿#ifndef GAMEVIEW_H
+#define GAMEVIEW_H
+
+#include <SFML/Graphics.hpp>
+
+struct GameView
+{
+    sf::RenderWindow window;
+    sf::View camera;
+    sf::Vector2i windowSize;
+    sf::Clock clock;
+};
+
+typedef void (*OnUpdate)(void* pData, GameView& view, float deltaSec);
+typedef void (*OnDraw)(void* pData, GameView& view);
+
+GameView* NewGameView(const sf::Vector2i& windowSize);
+
+void EnterGameLoop(GameView& view, OnUpdate onUpdate, OnDraw onDraw, void* pData);
+
+void SetCameraCenter(GameView& view, const sf::Vector2f& center);
+
+void DestroyGameView(GameView*& pView);
+
+#endif
